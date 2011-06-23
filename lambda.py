87f8963181807ca1db21bfcade75ada54332585f
Jeroen Ketema <j.ketema@ewi.utwente.nl>
2011-06-23 20:00:21 +0200

    Visualization with ubigraph
    Requires ubigraph.py

--- lambda.py
+++ lambda.py
@@ -1,5 +1,6 @@
 import sys
 import lambda_parser
+import ubigraph
 
 from collections import deque
 from LambdaTermClass import LambdaAbs, LambdaApp, LambdaVar
@@ -19,6 +20,33 @@ def iterate(term):
                 seen[reduct] = level + 1
                 todo.append((reduct, level + 1))
 
+def ubi_iterate(term):
+    U = ubigraph.Ubigraph()
+    U.clear();
+
+    smallRed = U.newVertexStyle(shape="sphere", color="#ff0000", size="1.0")
+    x = U.newVertex(style = smallRed, label = str(term))
+
+    seen = {term : x}
+    todo = deque([(term, x)])
+
+    while todo != deque([]):
+        (term, x_old) = todo.popleft()
+        for position in term.getRedexPositions():
+            reduct = term.reduce(position)
+
+            if reduct == term:
+                continue
+
+            if reduct not in seen:
+                x = U.newVertex(style = smallRed)
+                U.newEdge(x_old, x, width = "2.0", color = "#ffffff")
+                seen[reduct] = x
+                todo.append((reduct, x))
+            else:
+                x = seen[reduct]
+                U.newEdge(x_old, x, width = "2.0", color = "#ffffff")
+
 def omega_test():
     x     = LambdaVar(0)
     xx    = LambdaApp(x.copy(), x.copy())
@@ -26,6 +54,7 @@ def omega_test():
     omega = LambdaApp(delta.copy(), delta.copy())
 
     iterate(omega)
+    ubi_iterate(omega)
 
     print "Finished!"
 
@@ -37,7 +66,7 @@ def fixed_point_test():
     lfxx2 = LambdaApp(lfxx.copy(), lfxx.copy())
     Y     = LambdaAbs(lfxx2.copy())
 
-    iterate(Y)
+    ubi_iterate(Y)
 
     raise Exception("Should not be reachable!")
 
@@ -47,5 +76,6 @@ def fixed_point_test():
 print "Please input a lambda term: "
 line = sys.stdin.readline()
 term = lambda_parser.parse(line)
-iterate(term)
+#iterate(term)
+ubi_iterate(term)
 print "Finished"
